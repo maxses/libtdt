c59f9a354733cd29e125a199e95fd543f19edfe9
Maximilian Seesslen <mes@seesslen.net>
2024-12-29 13:23:46 +0100

    fixed duplicate function

--- src/printer.cpp
+++ src/printer.cpp
@@ -17,20 +17,14 @@
 namespace Tdt
 {
 
+
 EObject operator+(const EObject &a, int value)
 {
    return( (EObject)( (int)a + value ) );
 };
 
-int operator-(const EObject &a, EObject b)
-{
-   return( (int)a - (int)b );
-};
-
 
 }; // namespace Tdt
-
-//QMap<Tdt::EObject, const char *> CCanTdtPrinter::m_objectMap;
 
 
 //---fin-----------------------------------------------------------------------
