a9e95459654a7152197d5b397f1f4b2eccd52091
Maximilian Seesslen <mes@seesslen.net>
2025-01-28 13:13:11 +0100

    fixed compiler issues

---
+++ tests/test_main.cpp
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------------------
+///
+/// \file   test_main.cpp
+///
+/// \brief  main file unit tests
+///
+/// \date   20220619
+/// \author Maximilian Seesslen <mes@seesslen.net>
+///
+//----------------------------------------------------------------------------
+
+
+//---Documentation------------------------------------------------------------
+
+
+//---Includes -----------------------------------------------------------------
+
+
+//---General--------------------------
+
+#define CATCH_CONFIG_MAIN
+
+#if defined ( CATCH_V3 )
+   #include <catch2/catch_test_macros.hpp>
+#elif defined ( CATCH_V2 )
+   #include <catch2/catch.hpp>
+#elif defined ( CATCH_V1 )
+   #include <catch/catch.hpp>
+#else
+   #error "Either 'catch' or 'catch2' has to be installed"
+#endif
+
+
+//---fin-----------------------------------------------------------------------
