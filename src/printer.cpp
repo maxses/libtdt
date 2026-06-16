0d2f26cfc9347dcc6ca55d20d83f4fa31a7cd5c4
Maximilian Seesslen <dev@seesslen.net>
2026-06-16 21:11:59 +0200

    Fixed data taype in printer

--- src/printer.cpp
+++ src/printer.cpp
@@ -207,7 +207,7 @@ QString CPrinter::printValueGeneric( EFunctionCode functionCode, EUnit unit,
       {
          if( unitDesc.divider != 1 )
          {
-            float t = value._float;
+            float t = value._int;
             t /= (   // If unitDesc.decimalPower is '0' assume '1'
                      unitDesc.divider ? unitDesc.divider : 1 );
             s += QString("%1").arg( t );
