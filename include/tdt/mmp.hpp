28d9f20ecf8199684cb0b3fa29f6ab1bf0f11010
Maximilian Seesslen <dev@seesslen.net>
2026-06-27 17:17:56 +0200

    Fixed compiling when liblepto is not used

--- include/tdt/mmp.hpp
+++ include/tdt/mmp.hpp
@@ -47,10 +47,14 @@
 /*--- Declaration ----------------------------------------------------------*/
 
 
-#if defined( STM32 ) && IS_ENABLED( CONFIG_TDT_MMP_CALLBACKS )
-   void cbSendTdtMessage( const Tdt::CMessage& );
-   Tdt::nodeId_t cbGetNodeId();
-   int cbHandleMmpTransfer( const Tdt::CMmpTransferData& );
+#if USE_LEPTO
+   #if defined( STM32 ) && IS_ENABLED( CONFIG_TDT_MMP_CALLBACKS )
+      void cbSendTdtMessage( const Tdt::CMessage& );
+      Tdt::nodeId_t cbGetNodeId();
+      int cbHandleMmpTransfer( const Tdt::CMmpTransferData& );
+   #endif
+#else
+   #define IS_ENABLED( A ) A
 #endif
 
 namespace Tdt
