855683fd5422223b443eb093866e99b77d3edd95
Maximilian Seesslen <dev@seesslen.net>
2026-06-30 20:31:49 +0200

    Throw error when libbiwak is not used on STM32 devices

--- include/tdt/mmp.hpp
+++ include/tdt/mmp.hpp
@@ -39,6 +39,9 @@
       #error On STM32 libbiwak is needed. Maybe you have to move the "add_subdirectory" in your CMakeLists.txt.
    #endif
    #define slots
+   #if ! defined USE_BIWAK
+      #error TDT-MMP: On STM32 devices i need libbiwak. Is the order of submodules in CMakeLists.txt correct?
+   #endif
    #include <biwak/sysTimer.hpp>
    #include <biwak/softtimer.h>
 #else
