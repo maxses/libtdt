9db713883d7f00d6fa897fbb1b564c3c11b94445
Maximilian Seesslen <maximilian.seesslen@ifm.com>
2026-06-30 15:56:53 +0200

    Shrink binary size by using LDS

--- src/mmpNode.cpp
+++ src/mmpNode.cpp
@@ -466,8 +466,9 @@ void CMmpNode::finishTx( const Tdt::CMessage& msg )
    // Acknowledge is missing but i already got transfer ack ?
    if( ! m_tx.isFinished( m_tx.pos()+1 ) )
    {
-      qFatal("Transfer Ack on non finished ttransfer. Pos is %d, data size is %d."
-             , m_tx.pos(), m_tx.header().dataLength);
+      qFatal( LDS( "TACK NFIT %d/%d"
+            , "Transfer Ack on non finished ttransfer. Pos is %d, data size is %d.")
+            , m_tx.pos(), m_tx.header().dataLength);
    }
    assert( msg.getLen()>= 8 );
    m_tx.setReturnCode( (Tdt::EReturnCode)msg.getTdtValue()->_uint );
